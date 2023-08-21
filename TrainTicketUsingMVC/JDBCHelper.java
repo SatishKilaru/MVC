caec28eef0e4e9dc8c85596bf7c1f74f4f10db0a
Kilaru Satish <92021979+SatishKilaru@users.noreply.github.com>
2023-08-21 16:03:14 +0530

    Create JDBCHelper.java

---
+++ TrainTicketUsingMVC/JDBCHelper.java
@@ -0,0 +1,47 @@
+package satish;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JDBCHelper {
+	static String url = "jdbc:postgresql://192.168.110.48:5432/plf_training";
+	static String username = "plf_training_admin";
+	static String password = "pff123";
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, username, password);
+	}
+
+	public static void closeConnection(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void closePreparedStatement(PreparedStatement preparedStatement) {
+		if (preparedStatement != null) {
+			try {
+				preparedStatement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet resultSet) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
