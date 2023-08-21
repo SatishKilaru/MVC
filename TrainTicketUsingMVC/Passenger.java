024344b213fbd7dd2358d81435eda9bff1ed2183
Kilaru Satish <92021979+SatishKilaru@users.noreply.github.com>
2023-08-21 16:03:45 +0530

    Create Passenger.java

---
+++ TrainTicketUsingMVC/Passenger.java
@@ -0,0 +1,39 @@
+package satish;
+
+public class Passenger {
+
+	private String name;
+	private String gender;
+	private String age;
+
+	public Passenger(String name, String gender, String age) {
+
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+}
